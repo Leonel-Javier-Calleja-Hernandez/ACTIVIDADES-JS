4e183e71d919a33b590754003887cffceda182ba
Leonel-Javier-Calleja-Hernandez <leocallejas565@gmail.com>
2024-06-26 21:28:40 -0500

    Online class/class 5/inicio

---
+++ Online class/class 4/js/index.js
@@ -0,0 +1,10 @@
+var a = 1;
+var b = 2;
+// array normal
+var arrayNormal=["azul","leon"]
+// array asociatibos
+var arrayAsociativo={"color":"azul","animal":"leon"} 
+
+alert(`a es ${a} y b es ${b}`)
+alert(arrayAsociativo["color"])
+alert(arrayNormal[1])
